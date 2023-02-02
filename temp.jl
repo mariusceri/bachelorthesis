99b711c15105a403eb57d42414e69e40bcfdabef
Marius Ceribelli <mariusceribelli@gmail.com>
2023-02-02 13:21:19 +0100

    Replaced loops with functions

--- temp.jl
+++ temp.jl
@@ -1,88 +1,93 @@
 # Constants
 
-solarc = 1340
-alb = 0.32
-N = 20
-ps = 101325
-coefvis = 0.99
-coefir = 0.99
-eps = 0.92
-cpp = 1.0005
-g = 9.81
-c_sw = -.5*log(coefvis)/ps
-c_lw = -log(coefir)/sqrt(ps*ps/(2*g))
-stephan = 5.67e-8
+params = (solarc = 1340, alb = 0.32, ps=101325, coefvis=0.99, coefir = 0.1, eps = 0.9, cpp = 1.0005, g = 9.81, stephan = 5.67e-8)
+n = 10
+c_sw = -.5*log(params.coefvis)/params.ps
+c_lw = -log(params.coefir)/sqrt(params.ps*params.ps/(2*params.g))
 
-
-# Initialisation of arrays
-
-flux_down_sw = zeros(N+1)
-flux_up_sw = zeros(N+1)
-tau_0_sw = zeros(N+1)
-tau_inf_sw = zeros(N+1)
-p_int = zeros(N+1)
-
-temp = 293*ones(N)
 
 # Pressure
 
-for i in 1:N+1
-    p_int[i]=ps*(1-(i/N))
+p_int = zeros(n+1)
+
+for i in 1:n+1
+    p_int[i]=params.ps*(1-(i/(n+1)))
 end
 
 # Black body radiation
 
 function planck(T)
-    stephan*(T)^4
+    params.stephan*(T)^4
 end
 
-# Computation of shortwave fluxes
+# Shortwave
 
-for i in 1:N+1
+flux_down_sw = zeros(n+1)
+flux_up_sw = zeros(n+1)
+tau_0_sw = zeros(n+1)
+tau_inf_sw = zeros(n+1)
+
+for i in 1:n+1
     tau_inf_sw[i] = exp(-c_sw*p_int[i])
-    flux_down_sw[i] = tau_inf_sw[i]*solarc
-end 
-
-for i in 1:N+1
-    tau_0_sw[i] = exp(-(c_sw*ps+c_sw*p_int[i]))
-    flux_up_sw[i] = alb*tau_0_sw[i]*flux_down_sw[1]
+    flux_down_sw[i] = tau_inf_sw[i]*params.solarc
+    tau_0_sw[i] = exp(-(c_sw*params.ps+c_sw*p_int[i]))
+    flux_up_sw[i] = params.alb*tau_0_sw[i]*flux_down_sw[1]
 end
 
 flux_tot_sw = flux_up_sw - flux_down_sw
+    
+# Longwave
 
 function tau(i_1, i_2)
     exp(-sqrt(abs(c_lw*c_lw*(p_int[i_2]*p_int[i_2]-p_int[i_1]*p_int[i_1]))))
 end
 
+
+
+function lw_down(flux_down_lw, temp)
+    for i in 1:n+1
+        for k in i+1:n
+            flux_down_lw[i] += (tau(i,k) - tau(i, k+1))*planck(temp[k])
+        end
+    end
+    return flux_down_lw
+end
+
+function new_ts(flux)
+    return sqrt(sqrt(flux[1]/params.stephan))
+end
+
+function lw_up(flux_down , t_s, temp, flux_up_lw=zeros(n+1))
+    flux_up_lw[1] = params.eps*params.stephan*t_s^4 + (1-params.eps)*flux_down[1]
+    for i in 2:n+1
+        for k in 1:i-1
+            flux_up_lw[i] += (tau(k+1,i) - tau(k,i))*planck(temp[k]) + tau(1,i)*flux_up_lw[1]
+        end
+    end
+    return flux_up_lw
+end
+
+
 # Changes in temperature 
 
-function temp_ev(t_f, t=0, temp = 293*ones(N))
-    dt = 50
+function temp_ev(t_f,t=0, temp = 293*ones(n))
+    dt = 10
     while t < t_f
-        flux_down_lw = zeros(N+1)
-        flux_up_lw = zeros(N+1)
-        dT_sw = zeros(N)
-        dT_lw = zeros(N)
-        for i in 1:N+1
-            for k in i+1:N
-                flux_down_lw[i] += (tau(i,k) - tau(i, k+1))*planck(temp[k]) 
-            end 
-        end 
-        
-        for i in 1:N+1
-            for k in 1:i-1
-                flux_up_lw[i] += (tau(k+1,i) - tau(k,i))*planck(temp[k]) + tau(1,i)*(eps*planck(temp[1]) + (1-eps)*flux_down_lw[1])
-            end
-        end
+        flux_down_lw = lw_down(zeros(n+1), temp)
+        t_s = sqrt(sqrt(flux_down_lw[1]/params.stephan))
+        flux_up_lw = lw_up(flux_down_lw, t_s, temp)
 
         flux_tot_lw = flux_up_lw - flux_down_lw
 
-        for i in 1:N
-            dT_sw[i] = g*(flux_tot_sw[i] - flux_tot_sw[i+1])/(cpp*(p_int[i]-p_int[i+1]))
-            dT_lw[i] = g*(flux_tot_lw[i+1]-flux_tot_lw[i])/(cpp*(p_int[i+1]-p_int[i]))
+        dT_sw = zeros(n)
+        dT_lw = zeros(n)
+
+        for i in 1:n
+            dT_sw[i] = params.g*(flux_tot_sw[i] - flux_tot_sw[i+1])/(params.cpp*(p_int[i]-p_int[i+1]))
+            dT_lw[i] = params.g*(flux_tot_lw[i+1]-flux_tot_lw[i])/(params.cpp*(p_int[i+1]-p_int[i]))
         end
         dT = dT_sw + dT_lw
-        for i in 1:N
+        for i in 1:n
             temp[i] += dt*dT[i]
         end
         t += dt
