c7670a8d7895976424396b905c7bd6ecf2671df0
Thomas Dubos <prenom.nom@lmd.ipsl.fr>
2023-02-17 19:09:16 +0100

    fix : Cp, Ts, flux_up_lw

--- temp.jl
+++ temp.jl
@@ -3,18 +3,20 @@ using(Plots)
 # Black body radiation
 
 function planck(T, params)
-    params.stephan*(T)^4
+    params.stephan*(T^4)
 end
 
 # Longwave
 
-function tau_lw(i_1, i_2, params)
-    exp(-sqrt(abs((params.c_lw)^2*((params.p_int[i_2])^2-(params.p_int[i_1])^2))))
+function tau_lw(i_1, i_2, params) # i_1 > i_2
+#    exp(-sqrt(abs((params.c_lw)^2*((params.p_int[i_2])^2-(params.p_int[i_1])^2))))
+    zdup = ((params.p_int[i_2])^2-(params.p_int[i_1])^2)/(2*params.g)
+    return exp(-params.c_lw*sqrt(zdup))
 end
 
 function lw_down(flux_down_lw, temp, params)
     for i in 1:params.n +1
-        for k in params.n:-1:i
+        for k in params.n:-1:i  # k>=i, OK            
             flux_down_lw[i] += (tau_lw(k,i, params) - tau_lw(k+1, i, params))*planck(temp[k], params)
         end
     end
@@ -23,10 +25,11 @@ end
 
 function lw_up(flux_down , t_s, temp, params)
     flux_up_lw = zeros(params.n +1)
-    flux_up_lw[1] = params.eps*params.stephan*t_s^4 + (1-params.eps)*flux_down[1]
-    for i in 2:params.n +1
-        for k in 1:i-1
-            flux_up_lw[i] += (tau_lw(i, k+1, params) - tau_lw(i,k, params))*planck(temp[k], params) + tau_lw(1,i, params)*flux_up_lw[1]
+    flux_up_lw[1] = params.eps*planck(t_s, params) + (1-params.eps)*flux_down[1]
+    for i in 2:params.n+1
+        flux_up_lw[i] = tau_lw(i,1,params)*flux_up_lw[1]
+        for k in 1:i-1 # i>=k+1
+            flux_up_lw[i] += (tau_lw(i, k+1, params) - tau_lw(i,k, params))*planck(temp[k], params)
         end
     end
     return flux_up_lw
@@ -36,7 +39,7 @@ end
 
 function temp_ev(t_f, params, flux_tot_sw)
     temp = 293*ones(params.n)
-    dt = 40
+    dt = 3600
     c = 10                               # number of curves to plot
     Y = zeros(c, params.n +1)            # initialisation of the vector for the plot   
     i = 1                                # iterator for the plot 
@@ -45,17 +48,27 @@ function temp_ev(t_f, params, flux_tot_sw)
     for t in 1:dt:t_f
 
         flux_down_lw = lw_down(zeros(params.n +1), temp, params)
-        t_s = sqrt(sqrt(flux_down_lw[1]/params.stephan))
+        # surface energy balance 
+        # flux_up_lw + flux_up_sw = flux_down_lw + flux_down_sw   
+        # (1-emissiv)*flux_down_lw + emiss * sigma Ts^4 + flux_up_sw = flux_down_lw + flux_down_sw
+        #  emiss * sigma Ts^4 = emissiv*flux_down_lw + flux_down_sw*(1-albedo)
+        t_s = sqrt(sqrt((flux_down_lw[1]-flux_tot_sw[1]/params.eps)/params.stephan))
+#        @info "surface lw flux" flux_down_lw[1]
+#        @info "surface temp" t_s
+
         flux_up_lw = lw_up(flux_down_lw, t_s, temp, params)
 
         flux_tot_lw = flux_up_lw - flux_down_lw
+
+#        @info "flux_lw" flux_down_lw flux_up_lw
+#        error()
 
         dT_sw = zeros(params.n)
         dT_lw = zeros(params.n)
 
         for i in 1:params.n
-            dT_sw[i] = params.g*(flux_tot_sw[i+1] - flux_tot_sw[i])/(params.cpp*(params.p_int[i+1]-params.p_int[i]))
-            dT_lw[i] = params.g*(flux_tot_lw[i+1]-flux_tot_lw[i])/(params.cpp*(params.p_int[i+1]-params.p_int[i]))
+            dT_sw[i] = params.g*(flux_tot_sw[i]-flux_tot_sw[i+1])/(params.cpp*(params.p_int[i]-params.p_int[i+1]))
+            dT_lw[i] = params.g*(flux_tot_lw[i]-flux_tot_lw[i+1])/(params.cpp*(params.p_int[i]-params.p_int[i+1]))
         end
 
         dT = dT_sw + dT_lw
@@ -75,9 +88,11 @@ end
 # Execution
 
 function main()
-    params = (n = 20, solarc = 1340, alb = 0.32, ps=101325, coefvis=0.9, coefir = 0.01, eps = 0.9, cpp = 1.0005, g = 9.81, stephan = 5.67e-8)
-    c_sw = -.5*log(params.coefvis)/params.ps
-    c_lw = -log(params.coefir)/sqrt((params.ps)^2/(2*params.g))
+    params = (n = 100, solarc = 1340/4, alb = 0.32, ps=101325, coefvis=0.9, coefir = 0.08, eps = 0.9, cpp = 1000.0005, g = 9.81, stephan = 5.67e-8)
+#    params = (n = 20, solarc = 1340/4, alb = 0.0, ps=101325, coefvis=1.0, coefir = 1.0, eps = 1.0, cpp = 1000.0005, g = 9.81, stephan = 5.67e-8)
+    c_sw = -.5*log(params.coefvis)/params.ps # tau0
+    c_lw = -log(params.coefir)/sqrt((params.ps)^2/(2*params.g)) # == zcoef
+    @info "absorption coefficients" c_sw c_lw
     params = (; params..., c_lw, c_sw)
 
     # Pressure
@@ -94,18 +109,16 @@ function main()
     tau_0_sw = zeros(params.n +1)
     tau_inf_sw = zeros(params.n +1)
 
-    
-
     for i in 1:params.n +1
         tau_inf_sw[i] = exp(-c_sw*p_int[i])
         flux_down_sw[i] = tau_inf_sw[i]*params.solarc
-        tau_0_sw[i] = exp(-(c_sw*params.ps+c_sw*p_int[i]))
+        tau_0_sw[i] = exp(-c_sw*(params.ps-p_int[i]))
         flux_up_sw[i] = params.alb*tau_0_sw[i]*flux_down_sw[1]
     end
 
     flux_tot_sw = flux_up_sw - flux_down_sw
 
-    temp_ev(100000, params, flux_tot_sw)
+    temp_ev(3600*10000, params, flux_tot_sw)
 end
 
 main()
